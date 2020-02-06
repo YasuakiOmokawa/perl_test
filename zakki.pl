6e7ec06fb766cc37914c7265a6537706d4142f16
面川泰明 <omokawa@senk-inc.co.jp>
2020-02-06 15:52:59 +0900

    sample regex update

--- zakki.pl
+++ zakki.pl
@@ -5,9 +5,9 @@ use warnings;
 use utf8;
 use Encode qw/encode decode/;
 
-my %h = qw(A な B ん D り C つ E い );
-
-print encode('UTF-8', "$h{A}$h{B}$h{C}$h{A}$h{D}$h{E}") . "\n";
-
-# use Data::Dumper;
-# print Dumper \%a;
+my @iphones = qw(iPhone6 iPhone6s iPhone7 iPhone7s);
+for my $iphone (@iphones) {
+  if ($iphone =~ /iPhone6s?/) {
+    print "it is target iPhone => $iphone\n";
+  }
+}
