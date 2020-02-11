00543b2e3d69e5f2c70689a6ac2eb465cc1fc0ff
面川泰明 <omokawa@senk-inc.co.jp>
2020-02-11 20:09:26 +0900

    change regex pattern

--- zakki.pl
+++ zakki.pl
@@ -5,9 +5,10 @@ use warnings;
 use utf8;
 use Encode qw/encode decode/;
 
-my @iphones = qw(iPhone6 iPhone6s iPhone7 iPhone7s);
-for my $iphone (@iphones) {
-  if ($iphone =~ /iPhone6s?/) {
-    print "it is target iPhone => $iphone\n";
-  }
+my @files = qw(f1.md f2.f2-1.md f3.f3-1.f3-2.log.bak f4.f4-1.f4-2.log.bak.bak2);
+for my $file (@files) {
+  print "-----------\n";
+  print "before => $file\n";
+  $file =~ s/(.+)\.[^.]+$/$1\.html/;
+  print "after  => $file\n";
 }
