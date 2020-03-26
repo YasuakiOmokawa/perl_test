28e2531f73c6184ad63f88578bd1422673da6ec9
面川泰明 <omokawa@senk-inc.co.jp>
2020-03-26 23:51:50 +0900

    Change Script

--- proc_data.pl
+++ proc_data.pl
@@ -3,11 +3,16 @@ use strict;
 use warnings;
 
 my $sales = 0;
+my $is_header = 0;
 while (my $line = <>) {
   chomp $line;
   my @row = split /,/, $line;
 
+  if ($is_header) {
+    print "$row[0], $row[2]\n";
+
+  }
+
   $sales += $row[2];
-  print "$row[0], $row[2]\n";
   print "$row[0], $sales\n";
 }
