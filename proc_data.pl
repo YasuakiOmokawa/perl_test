b36c3d0597223205f87f08c6db26207b7ec8386f
面川泰明 <omokawa@senk-inc.co.jp>
2020-03-27 09:04:17 +0900

    Add header check

--- proc_data.pl
+++ proc_data.pl
@@ -3,14 +3,15 @@ use strict;
 use warnings;
 
 my $sales = 0;
-my $is_header = 0;
+my $is_header = 1;
 while (my $line = <>) {
   chomp $line;
   my @row = split /,/, $line;
 
   if ($is_header) {
     print "$row[0], $row[2]\n";
-
+    $is_header = 0;
+    next;
   }
 
   $sales += $row[2];
